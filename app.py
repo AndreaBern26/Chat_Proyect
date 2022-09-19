a87a3b21f4a4e3c22d693225af45c43a005ea1f3
Palatta <alib.shanti@gmail.com>
2022-09-19 15:29:50 +0200

    environment setup

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
 
-#Routes
-@app.route("/")
-def home():
-    return "index.html"
+app = Flask(__name__, template_folder="templates")
 
+@app.get("/login")
+def login_template():
+    return render_template("login.html")
+
+@app.route('/')
+def hello_world():
+    return "Hello world!"
+
+
+if __name__ == "__main__":
+    app.run()
