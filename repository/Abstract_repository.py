327a7fd7879c3c1da52020c5f62b78f5c4e5d6bf
AndreaBern26 <101724840+AndreaBern26@users.noreply.github.com>
2022-10-10 12:33:23 +0200

    fix abstract_repository

--- repository/Abstract_repository.py
+++ repository/Abstract_repository.py
@@ -3,10 +3,10 @@ import os
 
 from models.base import Base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker,scoped_session
 
 class AbstractRepository(abc.ABC):
     engine = create_engine(os.environ.get('DATABASE_URL'))
     Base.metadata.create_all(engine, checkfirst = True)
-    Session = sessionmaker(bind = engine)
+    Session = scoped_session(sessionmaker(bind = engine))
     session = Session()
