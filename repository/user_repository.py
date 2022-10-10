6f2e32d989ddf37aab815dbe30a84e89c57e7fbd
AndreaBern26 <101724840+AndreaBern26@users.noreply.github.com>
2022-10-10 12:59:43 +0200

    created function to get user by username

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -5,10 +5,13 @@ from repository.abstract_repository import AbstractRepository
 
 class UserRepository(AbstractRepository):
     def get(self, user_id):
-        return self.session.query(User).filter_by(id = user_id).one_or_none()
+        return self.session.query(User).filter_by(id=user_id).one_or_none()
     
     def get_user_by_email(self, email):
-        return self.session.query(User).filter_by(email = email).one_or_none()
+        return self.session.query(User).filter_by(email=email).one_or_none()
+    
+    def get_user_by_username(self, username):
+        return self.session.query(User).filter_by(username=username).one_or_none()
     
     def list(self):
         return self.session.query(User).all()
