27d6b926a57610de3d01113296ccbed1691d7991
AndreaBern26 <101724840+AndreaBern26@users.noreply.github.com>
2022-10-11 20:49:52 +0200

    adding delete endpoints

--- blueprints/global_message.py
+++ blueprints/global_message.py
@@ -2,14 +2,10 @@ from flask import Blueprint, render_template, make_response
 from flask_login import current_user, login_required
 
 from forms.global_chat_form import GlobalForm
-from flask import Blueprint, render_template, make_response
-from flask_login import current_user, login_required
-
-from forms.global_chat_form import GlobalForm
 from models.global_message import GlobalMessage
 from repository.g_message_repository import GlobalMessageRepository
 
-g_message = Blueprint('global', __name__, url_prefix='/global')
+g_message = Blueprint('global',__name__, url_prefix='/global')
 
 @g_message.get('/')
 @login_required
@@ -45,3 +41,23 @@ def global_chat():
         
     except Exception as e:
         return make_response(e.__str__(),400)
+
+@g_message.delete('/delete/<g_message_id>')
+@login_required
+def delete_g_message(g_message_id):
+    try:
+        g_message_repository = GlobalMessageRepository()
+        g_message = g_message_repository.get(g_message_id)
+        
+        if g_message is None:
+            raise ValueError("The message doesn't exist")
+
+        if g_message.user_id != current_user.id:
+            raise ValueError("Unauthorized", 401)
+        
+        g_message_repository.delete(g_message)
+
+        return make_response('Message deleted', 200)
+    
+    except Exception as e:
+        return make_response(e.__str__(), 400)
--- blueprints/users.py
+++ blueprints/users.py
@@ -13,3 +13,47 @@ def get_users():
     list_users = [user.to_json('list') for user in user_list]
 
     return make_response(list_users, 200)
+
+from flask import Blueprint, make_response
+from flask_login import current_user, login_required
+
+from repository.user_repository import UserRepository
+
+users = Blueprint('users', __name__, template_folder='templates', url_prefix='/users')
+
+@users.get('/')
+@login_required
+def get_users():
+    user_repository = UserRepository()
+    user_list = user_repository.list()
+    list_users = [user.to_json('list') for user in user_list]
+    
+    return make_response(list_users,200)
+
+@users.get('/<id>')
+@login_required
+def get_user(id):
+    try:
+        user_repository = UserRepository()
+        user = user_repository.get(id)
+        return make_response(user, 200)
+    except Exception as e:
+        return make_response(e.__str__(), 400)
+
+@users.delete('/delete/<user_id>')
+@login_required
+def delete_user(user_id):
+    try:
+        user_repository = UserRepository()
+        user_del = user_repository.get(user_id)
+
+        if user_del is None:
+            raise ValueError(f"The user: {user_id} doesn't exist", 400)
+                
+        if user_del != current_user.id:
+            raise ValueError("Unauthorized", 401)
+        
+        user_repository.delete(user_del)
+        return make_response('user deleted', 200)
+    except Exception as e:
+        return make_response(e.__str__(), 400)
